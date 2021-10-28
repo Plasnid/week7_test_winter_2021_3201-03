b0a439982c79be40894cfd8809dde9f0481316ed
Plasnid <stuart@sackler.ca>
2021-10-28 13:06:24 -0400

    initial file commit

---
+++ script.js
@@ -0,0 +1,42 @@
+/**
+ * ! Section 1: Server setup - (10 points)
+ * * Complete the lite-server setup and console log the steps by describing them in an array and looping through your array
+*/
+
+/**
+ * ! Section 2: Classes - (40 points)
+ * * Create the following classes
+ * ! ES5 Class: A human
+ * *    -the human must:
+ * !        -take in 4 parameters
+ * *            -height(a number)
+ * *            -native language(a string, can be real or made up)
+ * *            -limbs(a descriptive object)
+ * *            -has good balance(a boolean)
+ * !        -have the following functions
+ * *            -hop on one foot
+ * *                -check if the instance of human has good balance.  If they do not, console log them falling over
+ * *            -a function that will:
+ * *                -dynamically add a paragraph to the html page showing the number of limb, language, and height
+ * 
+ * ! ES5 Class: A cryptid(strange monster) that extends from the human
+ * *    -the cryptid must extend from the human
+ * !        -the cryptid must take in the following additional parameters
+ * *            -type of covering(scales? skin? feathers? hair? Slime?)
+ * *            -name of the cryptid species
+ * *            -special cryptid powers(an arraay of strings)
+ * !        -the cryptid must have the following function
+ * *            -a function for describing the changing into a cryptid, and displaying that change as text in an h2 tag
+ * 
+ * !  Make an instance of each class, and run each function
+ */
+
+/**
+ * !Section 3: External Data, fetch, and console - (30 points)
+ * * Create a json data file on 1 of the following topics
+ * *    -array of objects describing tasks and their difficulty(at least 5 different tasks)
+ * *    -array of objects describing animals, their diets, and home environments(at least 5)
+ * *    -an array of objects describing books, their genre, titles, and authors(at least 5)
+ * 
+ * * Use the fetch command to load the data, parse it and dynamically list the contents using semantic html and loops.  Make each entry clickable. On clicking an entry, console log information about the entry.  Ensure your console log is styled with colour and size. 
+ */
